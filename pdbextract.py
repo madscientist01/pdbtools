77de40ee6791aa1cb950830b60bde130e7eb5dc1
madscientist01 <suk.namgoong@gmail.com>
2012-11-04 19:49:00 -0500

    Remove Biopython PDB Parser

--- pdbextract.py
+++ pdbextract.py
@@ -5,34 +5,6 @@
 #
 import sys, subprocess, re, os, glob, argparse
 import warnings
-# from Bio.PDB.PDBParser import PDBParser
-# from Bio.PDB import PDBIO
-# from Bio.PDB.PDBIO import Select
-
-# class ChainSelect(Select):
-# 	def __init__(self,targetchain):
-# 		self.targetchain = targetchain
-
-# 	def accept_chain(self, chain):
-# #       print dir(residue)
-# 		if chain.get_id() in self.targetchain:
-# 			return 1
-# 		else:
-# 			return 0
-
-# def PDBHeader(pdb):
-# 	header = []
-# 	idcapture = re.compile("^(\S+)")
-# 	capturelist = ["HEADER","TITLE","COMPND","SOURCE","KEYWDS","EXPDTA","AUTHOR","REVDAT","JRNL","REMARK","DBREF","SEQRES","MODRES","HET","HETNAM","HETSYN","FORMUL","HELIX","SHEET","LINK","SITE","CRYST1","ORIGX1","ORIGX2","ORIGX3","SCALE1","SCALE2","SCALE3"]
-# 	if os.path.exists(pdb):
-# 		f = open(pdb)
-# 		line = f.readline()
-# 		while line:
-# 			line = f.readline()
-# 			match = idcapture.match(line)
-# 			if match and match.group(1) in capturelist:
-# 				header.append(line)
-# 	return(header)
 
 def PDBParse(pdb,argument):
 	headers = []
@@ -98,62 +70,6 @@ def pdbProcess(filename, argument):
 			f.writelines(chains[chain])
 			f.close()
 	return ()
-
-
-
-# def pdbProcessing(filename, argument):
-# #
-# # Parse pdb content and extract using regex
-# # Using Biopython PDB parser
-
-# 	warnings.filterwarnings('ignore')
-# 	parser = PDBParser()
-# 	structure = parser.get_structure('self', filename)
-# 	header = PDBHeader(filename)
-# 	model  = structure[0]
-# 	print "processing {0}".format(filename)
-# 	if argument.extract:
-# 		chainlist = argument.extract.split()
-# 	if argument.exclude:
-# 		chainlist = []
-# 		excludelist = argument.exclude.split()
-# 		for chain in model:
-# 			if not chain.get_id() in excludelist:
-# 				chainlist.append(chain.get_id())
-# 	if argument.split:
-# 		splitlist = ''
-# 		for chain in model:
-# 			splitlist= splitlist+chain.get_id()
-
-# 	if argument.extract or argument.exclude:
-# 		if len(chainlist)>0:
-# 			w = PDBIO()
-# 			w.set_structure(structure)
-# 			w.save("temp.pdb",select=ChainSelect(chainlist))
-# 			f = open(filename[:len(filename)-4]+"_"+"".join(chainlist)+".pdb",'w')
-# 			f.writelines(header)
-# 			t = open("temp.pdb")
-# 			f.writelines(t.readlines())
-# 			t.close()
-# 			os.remove("temp.pdb")
-# 			f.close()
-					
-# 	if argument.split:
-# 		for chain in splitlist:
-# 			w = PDBIO()
-# 			w.set_structure(structure)
-# 			templist=[]
-# 			templist.append(chain)
-# 			w.save("temp.pdb",select=ChainSelect(templist))
-# 			f = open(filename[:len(filename)-4]+"_"+chain+".pdb",'w')
-# 			f.writelines(header)
-# 			t = open("temp.pdb")
-# 			f.writelines(t.readlines())
-# 			t.close()
-# 			os.remove("temp.pdb")
-# 			f.close()
-	
-# 	return ()
 
 def main(argument):
 
